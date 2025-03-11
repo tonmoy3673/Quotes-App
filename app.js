79774f76588586c4c3411b2f343b64bbcdd11156
MD. Mehedi Hasan Tonmoy <107616839+tonmoy3673@users.noreply.github.com>
2025-03-11 11:09:09 +0600

    :bulb: implement quotes app

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 console.log('connected');
+
+// ======== get Quotes by ID ========//
+let quote = document.getElementById('quote');
+let author = document.getElementById('author');
+
+// =========== getQuote =======//
+const url='https://dummyjson.com/quotes/random'
+const getQuotes=async()=>{
+    const res= await fetch(url);
+    const  data = await res.json();
+    console.log(data);
+    quote.innerText=data?.quote
+    author.innerText=data?.author
+}
+
+// ======= initial function call =======//
+getQuotes();
